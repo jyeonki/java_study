8e7470e11194844b7cfa6d744881eb2086cd0069
jyeonki <kay8825@naver.com>
2022-06-10 18:02:07 +0900

    [220610] java stack 문제풀이

--- src/datastructure/chap05/quiz/오큰수.java
+++ src/datastructure/chap05/quiz/오큰수.java
@@ -43,21 +43,45 @@ public class 오큰수 {
 
 
 
+        /* 풀이1
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N]; // 수열 배열
+        int[] answer = new int[N]; // 정답 배열
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0); // 처음에는 스택이 비어있으므로 항상 최초값을 추가
+
+        // 3 5 2 7    || 5 7 7 -1
+        for (int i = 1; i < N; i++) {
+
+            while (!stack.isEmpty() && A[stack.peek()] < A[i]) {
+                answer[stack.pop()] = A[i]; // 정답 배열에 오큰수를 저장
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            answer[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int n : answer) {
+            sb.append(n).append(" ");
+        }
+        System.out.println(sb);
+        */
 
 
 
+        /* 풀이2
 
-
-
-
-
-
-
-
-
-
-
-        /*
         Scanner in = new Scanner(System.in);
         Stack<Integer> stack = new Stack<Integer>();
 
