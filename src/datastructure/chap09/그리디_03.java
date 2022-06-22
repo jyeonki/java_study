242dc132a96545a6831c194758a14b147ed00e27
jyeonki <kay8825@naver.com>
2022-06-22 11:14:43 +0900

    [220622] java 그리디

--- src/datastructure/chap09/그리디_03.java
+++ src/datastructure/chap09/그리디_03.java
@@ -1,6 +1,5 @@
 package datastructure.chap09;
 
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -36,5 +35,24 @@ public class 그리디_03 {
 
         System.out.println("total = " + total);
 
+
+
+        /*
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 카드 묶음 수
+        PriorityQueue<Integer> cardList = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            cardList.add(sc.nextInt());
+        }
+
+        int total = 0;
+        while (cardList.size() != 1) {
+            int data1 = cardList.poll();
+            int data2 = cardList.poll();
+            total += data1 + data2;
+            cardList.offer(data1 + data2);
+        }
+        System.out.println(total);
+        */
     }
 }
--- src/datastructure/chap09/그리디_04.java
+++ src/datastructure/chap09/그리디_04.java
@@ -1,6 +1,4 @@
 package datastructure.chap09;
-
-
 /*
    0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
      <----->          =>   1회
@@ -22,6 +20,75 @@ package datastructure.chap09;
               따라서 종료시간이 같은 경우 시작시간을 먼저 배치하도록 정렬해야 한다.
  */
 
-// 백준 1913
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// 백준 1931
 public class 그리디_04 {
+
+    public static void main(String[] args) {
+
+        List<List<Integer>> A = new ArrayList<>();
+        A.add(new ArrayList<Integer>(Arrays.asList(0, 6)));
+        A.add(new ArrayList<Integer>(Arrays.asList(3, 5)));
+//        A.add(new ArrayList<Integer>(Arrays.asList(4, 4)));
+        A.add(new ArrayList<Integer>(Arrays.asList(1, 4)));
+        A.add(new ArrayList<Integer>(Arrays.asList(5, 9)));
+        A.add(new ArrayList<Integer>(Arrays.asList(5, 7)));
+        A.add(new ArrayList<Integer>(Arrays.asList(3, 8)));
+        A.add(new ArrayList<Integer>(Arrays.asList(8, 11)));
+        A.add(new ArrayList<Integer>(Arrays.asList(6, 10)));
+        A.add(new ArrayList<Integer>(Arrays.asList(2, 13)));
+        A.add(new ArrayList<Integer>(Arrays.asList(8, 12)));
+        A.add(new ArrayList<Integer>(Arrays.asList(12, 14)));
+
+        // 종료시간 기준 정렬
+        A.sort((nList1, nList2) -> {
+            if (nList1.get(1) == nList2.get(1)) {
+                return nList1.get(0) - nList2.get(0);
+            }
+            return nList1.get(1) - nList2.get(1);
+        });
+
+
+        for (List<Integer> nums : A) {
+            System.out.println(nums);
+        }
+
+
+
+
+        /*
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        List<List<Integer>> A = new ArrayList<>();
+
+        // 2차원 리스트에 데이터 입력받기
+        for (int i = 0; i < N; i++) {
+            A.add(new ArrayList<>());
+            A.get(i).add(sc.nextInt());
+            A.get(i).add(sc.nextInt());
+        }
+        sc.close();
+
+        // 정렬
+        A.sort((o1, o2) -> {
+            if (o1.get(1) == o2.get(1)) { // 종료 시간이 같으면
+                return o1.get(0) - o2.get(0); // 시작 시간으로 정렬
+            }
+            return o1.get(1) - o2.get(1); // 종료시간으로 정렬
+        });
+
+        int count = 0;  // 회의 횟수
+        int end = -1;   // 종료시간 저장
+        for (int i = 0; i < N; i++) {
+            if (A.get(i).get(0) >= end) { // 시작시간이 기존 종료시간보다 크거나같으면
+                end = A.get(i).get(1); // 종료시간을 갱신
+                count++;
+            }
+        }
+        System.out.println(count);
+        */
+    }
 }
